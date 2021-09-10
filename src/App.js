315b9d63a3496aadec05b6207cb480623c01902d
scarter31191 <scarter31191@gmaile.com>
2021-09-09 22:17:43 -0400

    created simple useState counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import React, {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
+
+
   return (
     <div className="App">
-      
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <span>{count}</span>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </div>
   );
 }
