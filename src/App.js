4326fc95e7c4899c0a8319d9fbfdf9280d82e9ab
scarter31191 <scarter31191@gmaile.com>
2021-09-09 22:28:01 -0400

    refactored useState

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import React, {useState} from "react";
 function App() {
   const [count, setCount] = useState(0)
 
+  function down(){
+    setCount(prevCount => prevCount - 1)
+  }
+
+  function up(){
+    setCount(prevCount => prevCount + 1)
+  }
 
   return (
     <div className="App">
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={up}>+</button>
       <span>{count}</span>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={down}>-</button>
     </div>
   );
 }
